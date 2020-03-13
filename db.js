80d249b35f6ce2872d333c2c5badb89d7b018a22
Roheemah <60899500+AbolareRoheemah@users.noreply.github.com>
2020-03-13 16:19:02 +0100

    Add files via upload
    Student's database

---
+++ db.js
@@ -0,0 +1,21 @@
+//exports.data = {'name':'Roheemah', 'password':'Allah'};
+exports.student1 = {
+    'name':'Kifaaya', 'password':'islam',
+'age':45, 'matric_no':'1549048', 'dept':'chemical eng','DOB':'19/10/5'
+}
+exports.student2 = {
+    'name':'Kifl', 'password':'muh',
+'age':67, 'matric_no':'1599046', 'dept':'chem eng','DOB':'1/8/5'
+}
+exports.student3 = {
+    'name':'faaya', 'password':'munn',
+'age':5, 'matric_no':'1576678', 'dept':'civil eng','DOB':'9/10/9'
+}
+exports.student4 = {
+    'name':'aay', 'password':'kurd',
+'age':75, 'matric_no':'1879948', 'dept':'systems eng','DOB':'16/8/05'
+}
+exports.student5 = {
+    'name':'sukaynah', 'password':'muslim',
+'age':18, 'matric_no':'18099808', 'dept':'mech eng','DOB':'19/12/06'
+}
